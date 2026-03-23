088f5e00332142dced700a90bb41bb94e74fa7d3
REEM <reemalthibani46@gmail.com>
2026-03-23 22:26:12 +0300

    solve the problem of the notification not appearing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hijri/hijri_calendar.dart';
@@ -9,6 +10,8 @@ import 'package:yusr/yusr_app.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   // 1. تفعيل اللغة العربية لكل التطبيق
   HijriCalendar.setLocal('ar');
 
@@ -16,11 +19,20 @@ void main() async {
   // إذا اكتشفت أن التاريخ في التطبيق متأخر بيوم عن الواقع، اجعلها 1
   // إذا كان صحيحاً اتركها 0
   // HijriCalendar.hAdjustment = 0;
-  WidgetsFlutterBinding.ensureInitialized();
 
   String initialRoute = AppRoute.mainHomeView;
+
   // 3. تهيئة فايربيس باستخدام الملف المولد
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  await messaging.requestPermission(alert: true, badge: true, sound: true);
+  try {
+    String? token = await FirebaseMessaging.instance.getToken();
+    print("FCM Token: $token");
+  } catch (e) {
+    print("Failed to get token: $e");
+  }
+
   // 2. تفعيل خدمة التقاط الضغطات والإشعارات التي برمجناها
   await PushNotificationService.init();
   // await Firebase.initializeApp();
